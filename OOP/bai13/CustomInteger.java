c09d7148a03e5eb5c69bd13a4f1c9ebee6fbb58f
tduckie_ <golden4gnvx@gmail.com>
2024-06-18 19:12:47 +0700

    adding bai13

---
+++ OOP/bai13/CustomInteger.java
@@ -0,0 +1,16 @@
+package Files.OOP.bai13;
+
+public class CustomInteger {
+    public int Value;
+    public boolean isEven;
+    public boolean isPrime;
+    public boolean isSymmetrical;
+
+    public int getValue() {
+        return Value;
+    }
+
+    public void setValue(int value) {
+        Value = value;
+    }
+}
---
+++ OOP/bai13/Main.java
@@ -0,0 +1,4 @@
+package Files.OOP.bai13;
+
+public class Main {
+}
